f7176bb0b217062e2762c37b90851d0baf05250a
rosg89 <rsuarezgalan@hotmail.com>
2022-05-14 23:33:41 -0300

    Se sube el proyecto

---
+++ main.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+height = input("Enter your height in m: ")
+weight = input("Enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+
+bmi = int(weight) / float(height)**2
+
+bmi_int = int(bmi)
+
+print(f"Your BMI is: {bmi_int}")
+
+
+
+
+
+
